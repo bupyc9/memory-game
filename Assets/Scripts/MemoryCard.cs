0ef1ddbf331913eb96104883b1fbaf12d02d6075
Afanasyev Pavel <bupyc9@gmail.com>
2017-09-10 20:28:38 +0300

    init

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] private GameObject cardBack;
+	[SerializeField] private SceneController controller;
+
+	private int _id;
+	public int id {
+		get { return _id; }
+	}
+
+	void OnMouseDown() {
+		if (cardBack.activeSelf) {
+			cardBack.SetActive (false);
+			controller.CardRevealed (this);
+		}
+	}
+
+	public void SetCard(int id, Sprite image) {
+		_id = id;
+		GetComponent<SpriteRenderer> ().sprite = image;
+	}
+
+	public void Unreveal() {
+		cardBack.SetActive (true);
+	}
+}
